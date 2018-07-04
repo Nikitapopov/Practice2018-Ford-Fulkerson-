013d3737bc92bbc94eb5a95028e030376c09495e
Nikita Popov <nikitso.popov@inbox.ru>
2018-07-04 17:40:22 +0300

    Time1.2. Final

--- com/etu/basic/Example2.java
+++
@@ -1,9 +0,0 @@
-package com.etu.basic;
-
-public class Example2 {
-
-    public static void main(String[] args) {
-        Example1 ref = new Example1(10);
-        System.out.println(ref.getField());
-    }
-}
